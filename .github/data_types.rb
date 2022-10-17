6adaaf8204b4e949da2667e016ac67c77e48b571
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-18 01:47:10 +0300

    ruby data types

---
+++ .github/data_types.rb
@@ -0,0 +1,29 @@
+#strings
+"I'm a string"
+'Me too!'
+
+
+# numbers
+7.5
+11
+
+#nil
+no_value=nil
+
+#booleans
+TrueClass
+FalseClass
+
+
+#symbols
+:symbol_name
+
+#arrays
+[1,40,82,230,450]
+
+#hashes
+my_hash = { key1: "value1", key2: "value2" }
+my_hash[:key2]
+
+#hash rocket syntax
+{ "i'm a key" => "i'm a value!", "key2" => "value2" }
